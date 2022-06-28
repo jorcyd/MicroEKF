f73547b6e92526b0d876452ec8f3eb34edbe79fc
Jorcy de Oliveira Neto <jorcyneto@petrobras.com.br>
2022-06-28 16:04:11 -0300

    improved approximation routines

--- src/ekf_math.c
+++ src/ekf_math.c
@@ -1,8 +1,15 @@
 #include "ekf_math.h"
 
-#ifdef EMBEDDED	//if set to run on embedded targets with light fp support.
+/* References to bit-hack based routines :
+ * https://github.com/hcs0/Hackers-Delight/blob/master/rsqrt.c.txt
+ * https://bits.stephan-brumme.com/squareRoot.html 
+ * https://github.com/hcs0/Hackers-Delight/blob/master/asqrt.c.txt
+ * https://bits.stephan-brumme.com/invSquareRoot.html 
+ * https://stackoverflow.com/questions/31117497/fastest-integer-square-root-in-the-least-amount-of-instructions
+ * https://stackoverflow.com/questions/31031223/fast-approximate-float-division	*/
 
-//https://github.com/hcs0/Hackers-Delight/blob/master/rsqrt.c.txt
+#ifdef EMBEDDED	//if set to run on embedded targets with basic fp support.
+
 inline float fast_rsqrtf(float x){	//1/sqrt(x)
 	// for Newton iteration
 	float xHalf = 0.5f*x;
@@ -15,7 +22,6 @@ inline float fast_rsqrtf(float x){	//1/sqrt(x)
 	//return x;
 }
 
-// https://stackoverflow.com/questions/31031223/fast-approximate-float-division
 inline float fast_inv(float x) {
 	union { float f; int i; } v;
 	float w;//, sx;
@@ -27,44 +33,21 @@ inline float fast_inv(float x) {
 	w = x * v.f;
 
 	// Efficient Iterative Approximation Improvement in horner polynomial form.
-	v.f = v.f * (2 - w);     // Single iteration, Err = -3.36e-3 * 2^(-flr(log2(x)))
-	// v.f = v.f * ( 4 + w * (-6 + w * (4 - w)));  // Second iteration, Err = -1.13e-5 * 2^(-flr(log2(x)))
-	// v.f = v.f * (8 + w * (-28 + w * (56 + w * (-70 + w *(56 + w * (-28 + w * (8 - w)))))));  // Third Iteration, Err = +-6.8e-8 *  2^(-flr(log2(x)))
+	//v.f = v.f * (2 - w);     					// Onte iteration, Err = -3.36e-3 * 2^(-flr(log2(x)))
+	v.f = v.f * ( 4 + w * (-6 + w * (4 - w)));  // Two iterations, Err = -1.13e-5 * 2^(-flr(log2(x)))
+	//v.f = v.f * (8 + w * (-28 + w * (56 + w * (-70 + w *(56 + w * (-28 + w * (8 - w)))))));  // Three Iteration, Err = +-6.8e-8 *  2^(-flr(log2(x)))
 	return v.f;//* sx;
 }
 
-/* 	Quick and dirty fast-float-sqrt 
-	https://bits.stephan-brumme.com/squareRoot.html 
-	https://github.com/hcs0/Hackers-Delight/blob/master/asqrt.c.txt
-	https://bits.stephan-brumme.com/invSquareRoot.html 
-	https://stackoverflow.com/questions/31117497/fastest-integer-square-root-in-the-least-amount-of-instructions
-	*/
 inline float fast_sqrtf(float x0) {
 	union {int ix; float x;} c;
 
-	c.x = x0;                      		// x can be viewed as int.
-	c.ix = 0x1fbb67a8 + (c.ix >> 1); 	// Initial guess.
-	c.x = 0.5f*(c.x + x0/c.x);         	// Newton step.
-	c.x = 0.5f*(c.x + x0/c.x);  		// 2nd iter (otherwise the estimates would suffer)
+	c.x = x0;                      			// x can be viewed as int.
+	c.ix = 0x1fbb67a8 + (c.ix >> 1); 		// Initial guess.
+	c.x = 0.5f*(c.x + x0*fast_inv(c.x));    // Newton step.
+	c.x = 0.5f*(c.x + x0*fast_inv(c.x));  // 2nd iter (required)
 	return c.x;
 }
-
-#if 0
-static inline float fast_sqrtf(float x) {
-	unsigned int i = *(unsigned int*) &x;
-	// adjust bias
-	i  += 127 << 23;
-	// approximation of square root
-	i >>= 1;
-	return *(float*) &i;
-}
-
-static inline float fast_sqrtf(float x) {
-	unsigned int i = *(unsigned int*) &x;
-	i = 0x1fbb4f2e + (i >> 1);
-	return *(float*) &i;
-}
-#endif
 
 #else
 
